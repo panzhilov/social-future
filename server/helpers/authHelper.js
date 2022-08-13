05fa17b8917a3263bfeb85e2ba7429051a8765e1
panzhilov <panzhilov@gmail.com>
2022-08-13 13:06:22 +0300

    initialize functionality for hash and compare passwords

--- server/helpers/authHelper.js
+++ server/helpers/authHelper.js
@@ -1,20 +1,26 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
-export const hashPassword = (password) => {
-    return new Promise((resolve, reject) => {
-        bcrypt.genSalt(11, (err, salt) => {
-            if(err) {
-                reject(err)
-            }
-            bcrypt.hash(
-                password, salt, 
-                (err, hashedPass) => {
-                    if(err) {
-                        reject(err)
-                    }
-                    resolve(hashedPass);
-                }
-                )
-        })
-    })
+function hashPassword(password) {
+  return new Promise((resolve, reject) => {
+    bcrypt.genSalt(11, (err, salt) => {
+      if (err) {
+        reject(err);
+      }
+      bcrypt.hash(password, salt, (err, hashedPass) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(hashedPass);
+      });
+    });
+  });
+}
+
+function comparePassword(password, hashed) {
+  return bcrypt.compare(password, hashed);
+}
+
+module.exports = {
+  hashPassword,
+  comparePassword
 }
