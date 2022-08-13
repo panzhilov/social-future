0739da64b3623793b6eb41162adfdf61168d2f6b
panzhilov <panzhilov@gmail.com>
2022-08-13 20:52:04 +0300

    mapping api routes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require('mongoose');
 const cors = require('cors');
+const fs = require('fs');
 
 const morgan = require('morgan')
 require('dotenv').config();
@@ -16,8 +17,15 @@ try {
   } catch (error) {
     console.log("Have problem with connecting to database");
   }
+
+
 // Middlewares
 app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
 app.use(cors());
+
+//Routes
+fs.readdirSync('./routes').map((route) => app.use('/api', require(`./routes/${route}`)));
+
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
