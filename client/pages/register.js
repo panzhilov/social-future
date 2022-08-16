c617299280dee63325888dbaa730f533bb416b71
panzhilov <panzhilov@gmail.com>
2022-08-16 11:22:36 +0300

    update register page

--- client/pages/register.js
+++ client/pages/register.js
@@ -1,10 +1,13 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { Modal } from "antd";
 import Link from "next/link";
+import { UserContext } from '../context';
 
 import AuthForm from "../components/forms/AuthForm";
+import { useRouter } from "next/router";
+
 
 const Register = () => {
   const [name, setName] = useState("");
@@ -14,14 +17,16 @@ const Register = () => {
   const [ok, setOk] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  const [state, setState] = useContext(UserContext);
+  const router = useRouter();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
       setLoading(true);
 
-      const result = await axios.post(
-        `${process.env.NEXT_PUBLIC_API}/register`,
+      const result = await axios.post(`/register`,
         {
           name,
           email,
@@ -41,6 +46,11 @@ const Register = () => {
       toast.error(err.response.data);
     }
   };
+
+  if(state && state.token){
+    router.push('/');
+  }
+
 
   return (
     <div className="container-fluid">
