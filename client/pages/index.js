148eb4868eb2ed98187176bbae62aa7ef3993c74
panzhilov <panzhilov@gmail.com>
2022-08-16 20:23:24 +0300

    initialize contex for user

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,15 @@
+import { useContext } from "react";
+import { UserContext } from "../context/index";
+
+
 const Home = () => {
+    const [state, setState] = useContext(UserContext);
     return(
         <div className="container">
             <div className="row">
                 <div className="col">
                     <h1 className="display-1 text-center py-5">Home Page</h1>
+                    {JSON.stringify(state)}
                     <img src="/images/main.jpg" alt="image" />
                 </div>
             </div>
