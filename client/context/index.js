4c3b6761d00942cb690ca020fe62a79533b6c8f0
panzhilov <panzhilov@gmail.com>
2022-08-16 11:25:05 +0300

    configure axios

--- client/context/index.js
+++ client/context/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import {useState, createContext, useEffect} from 'react';
 
 const UserContext = createContext();
@@ -11,6 +12,10 @@ const UserProvider = ({children}) => {
     useEffect(() => {
         setState(JSON.parse(localStorage.getItem('user')))
     }, []);
+
+    const token = state && state.token ? state.token : "";
+    axios.defaults.baseURL = process.env.NEXT_PUBLIC_API;
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
     return(
         <UserContext.Provider value={[state, setState]}>
