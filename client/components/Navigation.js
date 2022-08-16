cfa99e91177dd01c381a5393785db4f3f664086a
panzhilov <panzhilov@gmail.com>
2022-08-16 20:22:09 +0300

    update navigation

--- client/components/Navigation.js
+++ client/components/Navigation.js
@@ -1,24 +1,56 @@
-import Link  from "next/link";
+import Link from "next/link";
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../context";
+import { useRouter } from "next/router";
 
 const Navigation = () => {
-  return (
-    <nav className="nav d-flex justify-content-center">    
-      
-        <Link href="/">
-           <a className="nav-link text-light">Home</a>
-        </Link>
- 
-        <Link href="/login">
-           <a className="nav-link text-light">Login</a>
-        </Link>
-     
-        <Link href="/register">
-           <a className="nav-link text-light">Register</a>
-        </Link>
+  const [state, setState] = useContext(UserContext);
+  const[currentLink, setCurrentLink] = useState("")
+
+  useEffect(() => {
+    process.browser && setCurrentLink(location.pathname)
+  }, [process.browser && location.pathname])
+
+  console.log(currentLink);
   
+
+  const router = useRouter();
+
+  const logout = () => {
+    localStorage.removeItem("user");
+    setState(null);
+    router.push("/login");
+  };
+
+  return (
+    <nav className="nav d-flex justify-content-between">
+      <Link href="/">
+        <a className="nav-link text-light logo">SOCIAL FUTURE</a>
+      </Link>
+
+      {state !== null ? (
+        <>
+          <Link href="/user/dashboard">
+            <a className={`nav-link text-light ${currentLink == '/user/dashboard' && 'active'}`}>{state.user.name}</a>
+          </Link>
+
+          <a onClick={logout} className="nav-link text-light">
+            Logout
+          </a>
+        </>
+      ) : (
+        <>
+          <Link href="/login">
+            <a className={`nav-link text-light ${currentLink == '/login' && 'active'}`}>Login</a>
+          </Link>
+
+          <Link href="/register">
+            <a className={`nav-link text-light ${currentLink == '/register' && 'active'}`}>Register</a>
+          </Link>
+        </>
+      )}
     </nav>
   );
-  console.log('dsada');dsada
 };
 
 export default Navigation;
