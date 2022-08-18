c42a7ef4a73cdec54c48ef6651abc341237e69d6
panzhilov <panzhilov@gmail.com>
2022-08-18 11:35:51 +0300

    add time and date for createdAt and updatedAt

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,35 +1,38 @@
 const mongoose = require("mongoose");
 
-const postSchema = new mongoose.Schema({
-  content: {
-    required: true,
-    type: {},
-  },
-  image: {
-    url: String,
-    public_id: String,
-  },
-  likes: [
-    {
+const postSchema = new mongoose.Schema(
+  {
+    content: {
+      required: true,
+      type: {},
+    },
+    author: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
     },
-  ],
-  comments: [
-    {
-      text: String,
-      created: { type: Date, default: Date.now },
-      commentAuthor: {
+    image: {
+      url: String,
+      public_id: String,
+    },
+    likes: [
+      {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
       },
-    },
-  ],
-  author: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
+    ],
+    comments: [
+      {
+        text: String,
+        created: { type: Date, default: Date.now },
+        commentAuthor: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+      },
+    ],
   },
-});
+  { timestamps: true }
+);
 
 const Post = mongoose.model("Post", postSchema);
 
