4689435c3d95c84b9e83eb047d27d102c3bbbb77
panzhilov <panzhilov@gmail.com>
2022-08-18 21:55:12 +0300

    update routes - add for current post, update post and delete post

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const formidable = require('express-formidable');
 
-const { createPost, imageUpload, postsByUser } = require('../controllers/postController');
-const { requireSignin } = require('../middlewares/userMiddleware');
-
-
+const { createPost, imageUpload, postsByUser, userPost, updatePost, deletePost} = require('../controllers/postController');
+const { requireSignin, canEditDeletePost } = require('../middlewares/userMiddleware');
 
 
 const router = express.Router();
@@ -12,6 +10,9 @@ const router = express.Router();
 router.post('/create-post', requireSignin, createPost)
 router.post('/upload-image', requireSignin, formidable({maxFileSize: 5 * 1024 * 1024}), imageUpload)
 router.get('/user-posts', requireSignin, postsByUser)
+router.get('/user-post/:_id', requireSignin, userPost)
+router.put('/update-post/:_id', requireSignin, canEditDeletePost, updatePost);
+router.delete('/delete-post/:_id', requireSignin, canEditDeletePost, deletePost)
 
 
 
