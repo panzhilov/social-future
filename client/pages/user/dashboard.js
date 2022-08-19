9635eaf63160e0603ed685e266d9c5419a9ac43b
panzhilov <panzhilov@gmail.com>
2022-08-20 00:12:17 +0300

    Update dashboard.js

--- client/pages/user/dashboard.js
+++ client/pages/user/dashboard.js
@@ -5,13 +5,18 @@ import PostForm from "../../components/forms/PostForm.js";
 import { useRouter } from "next/router";
 import axios from "axios";
 import { toast } from "react-toastify";
-import PostList from "../../components/cards/PostLIst";
+import PostList from "../../components/cards/PostList";
+import { Modal } from "antd";
+import CommentForm from "../../components/forms/CommentForm";
 
 const Home = () => {
   const [state, setState] = useContext(UserContext);
   const [image, setImage] = useState({});
   const [uploading, setUploading] = useState(false);
   const [posts, setPosts] = useState([]);
+  const [comment, setComment] = useState("");
+  const [visible, setVisible] = useState(false);
+  const [currentPostComment, setCurrentPostComment] = useState({});
 
   const [content, setContent] = useState("");
 
@@ -74,19 +79,63 @@ const Home = () => {
 
   const handleDelete = async (post) => {
     try {
-      const answer = window.confirm('Are you sure delete this post?')
+      const answer = window.confirm("Are you sure delete this post?");
 
-      if(!answer) {
+      if (!answer) {
         return;
       }
-      
-      const {data} = await axios.delete(`/delete-post/${post._id}`);
-      toast.error('Post deleted')
+
+      const { data } = await axios.delete(`/delete-post/${post._id}`);
+      toast.error("Post deleted");
       fetchUserPosts();
     } catch (err) {
       console.log(err);
     }
-  }
+  };
+
+  const handleLike = async (_id) => {
+    try {
+      const { data } = await axios.put("/like-post", { _id });
+      console.log("liked", data);
+      fetchUserPosts();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleUnlike = async (_id) => {
+    try {
+      const { data } = await axios.put("/unlike-post", { _id });
+      console.log("unLiked", data);
+      fetchUserPosts();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleComment = (post) => {
+    setCurrentPostComment(post);
+    setVisible(true);
+  };
+
+  const addComment = async (e) => {
+    e.preventDefault();
+    
+    try {
+      const {data} = await axios.put('/add-comment', {
+        postId: currentPostComment._id,
+        comment
+      })
+      console.log('add comment', data);
+      setComment('');
+      setVisible(false);
+      fetchUserPosts();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const removeComment = async () => {};
 
   return (
     <UserRoute>
@@ -108,11 +157,30 @@ const Home = () => {
               image={image}
             />
             <br />
-            <PostList posts={posts} handleDelete={handleDelete} />
+            <PostList
+              posts={posts}
+              handleDelete={handleDelete}
+              handleLike={handleLike}
+              handleUnlike={handleUnlike}
+              handleComment={handleComment}
+            />
           </div>
 
           <div className="col-md-4">Sidebar</div>
         </div>
+
+        <Modal
+          visible={visible}
+          onCancel={() => setVisible(false)}
+          title="Comment"
+          footer={null}
+        >
+          <CommentForm
+            comment={comment}
+            setComment={setComment}
+            addComment={addComment}
+          />
+        </Modal>
       </div>
     </UserRoute>
   );
