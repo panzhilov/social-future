3bff63202913457a4b4388ac51191e5449409df5
panzhilov <panzhilov@gmail.com>
2022-08-20 17:12:45 +0300

    update index and import image

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,19 +1,38 @@
-import { useContext } from "react";
-import { UserContext } from "../context/index";
+import Link from "next/link";
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../context";
+import { useRouter } from "next/router";
+
 
 
 const Home = () => {
     const [state, setState] = useContext(UserContext);
+    
+    const router = useRouter();
+
+    const clickButton = () => {
+        if(state){
+            router.push('/user/dashboard')
+        } else{
+            router.push('/login')
+        }
+      };
     return(
-        <div className="container">
-            <div className="row">
-                <div className="col">
-                    <h1 className="display-1 text-center py-5">Home Page</h1>
-                    {JSON.stringify(state)}
-                    <img src="/images/main.jpg" alt="image" />
-                </div>
-            </div>
+        <>
+        <div className="container-fluid"
+        style={{
+            backgroundImage: "url('/images/home.png')",
+            backgroundAttachment: "fixed",
+            padding: "300px 0px 75px"
+        }}
+        >  
         </div>
+        <h1 className="display-1  text-center" style={{color: "#e63946"}}>Social future</h1>
+        <div className="display-4 text-center">
+            <button onClick={clickButton} className="p-3" style={{color: "#457b9d", borderRadius: "20%", border: "2px solid"}}>Let`s go</button>
+        </div>
+        </>
+       
     );
 }
 
